de908bd61980ecf8932ebc5def3b0c07f088e211
Andy Hunt <andy.l.hunt85@gmail.com>
2018-04-02 19:30:54 -0400

    finish twitch JS

--- script.js
+++ script.js
@@ -8,31 +8,60 @@ $(document).ready(function() {
     "esl_csgo"
   ]
 
-  $.each(channels, function(i, l) {
+
+  function apiInfo() {
+    var descriptions = document.getElementsByClassName('description');
+    var follower = document.getElementsByClassName('followers')
+    var views = document.getElementsByClassName('views')
+    var containers = document.getElementsByClassName('.channel-online')
+    var onOrOff = document.querySelectorAll('h5')
+    var container = document.getElementsByClassName('container')
+    var i = descriptions.length;
+
+    $.each(channels, function(i, l) {
 
     // API Call to Twitch, gets each URL
-    $.ajax( {
-      url: "https://wind-bow.glitch.me/twitch-api/streams/"  + channels[i] + "?callback=?",
-      type: "GET",
-      dataType: "jsonp",
-      headers: {
-        'Api-User-Agent': 'http://localhost:8080',
-        'Accept': 'application/vnd.twitchtv.v5+json'
-      },
+      $.ajax( {
+        url: "https://wind-bow.glitch.me/twitch-api/streams/"  + channels[i] + "?callback=?",
+        type: "GET",
+        dataType: "jsonp",
+        headers: {
+          'Api-User-Agent': 'http://localhost:8080',
+          'Accept': 'application/vnd.twitchtv.v5+json'
+        },
 
       // API Data Usage
-      success: function(data) {
-        console.log(data);
-        if (data.stream === null) {
-          $('.channel-online').addClass("offline")
-          console.log(channels[i])
-        } else {
-          $('.description').text(data.stream.channel.status);
-          $('.followers').text(data.stream.channel.followers);
-          $('.views').text(data.stream.channel.views);
+        success: function(data) {
+          if (data.stream == null) {
+            $(descriptions[i]).text('Offline')
+            $(container[i]).addClass('offline')
+          } else {
+            $(container[i]).addClass('online')
+            $(descriptions[i]).text(data.stream.channel.status);
+            $(follower[i]).text(data.stream.channel.followers);
+            $(views[i]).text(data.stream.channel.views);
+            $(onOrOff[i]).text('Now Online!')
+          }
         }
-      }
+      })
     })
+  }
+
+  $('#button-off').click(function() {
+    $('.offline').show();
+    $('.online').hide();
+  });
+
+  $('#button-on').click(function() {
+    $('.online').show();
+    $('.offline').hide();
+  });
+
+  $('#button-all').click(function() {
+    $('.wrapper').children().show();
   })
+
+  apiInfo()
+
 
 });
